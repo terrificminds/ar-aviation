b186796e81c82455fd4fa0330671628eefe8268e
Nisful Laila M. I <nisful@terrificminds.com>
2019-02-13 23:17:40 +0530

    ARA-75 #comment News and events section stylign completed

--- wp-content/themes/aviation/js/my-script.js
+++ wp-content/themes/aviation/js/my-script.js
@@ -1,8 +1,46 @@
 jQuery(document).ready(function ($) {
+
+  //To Instantiate Patners slider
   $("#partner-home").slick({
     dots: true,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1
   });
+
+  //Sticky Header
+  // $(window).scroll(function () {
+  //   var sticky = $('.site-header'),
+  //     scroll = $(window).scrollTop();
+
+  //   if (scroll >= 120) {
+  //     sticky.addClass('fixed');
+  //   }
+  //   else {
+  //     sticky.removeClass('fixed');
+  //   }
+  // });
+
+  //Hover interaction for news item
+  var width = $(window).width();
+  if (width > 1025) {
+    //desktop interaction
+    var toggleOn = false;
+    $(".news-block").hover(
+      function () {
+        $(this).addClass('active');
+      }, function () {
+        toggleOn || $(this).removeClass('active');
+      }
+    );
+  }
+  else {
+    // tablet and mobile interaction
+    $(".news-block").click(function () {
+      $(".news-block").removeClass('active');
+      $(this).toggleClass('active');
+
+    });
+  }
+
 });
--- wp-content/themes/aviation/template-parts/content-news.php
+++ wp-content/themes/aviation/template-parts/content-news.php
@@ -17,15 +17,20 @@
             <div class = "news-image">
                 <img class = "news-featured-image" src="<?php echo $featuredImage[0]; ?>">
             </div>
-            <div class = 'news-date'>
-                <?php echo $publishDate;?>
-            </div>
-            <div class = 'news-title'>
-                <?php echo get_the_title();?>
-            </div>
-            <div class = 'news-excerpt'>
-                <?php the_excerpt();?>
-            </div>               
+            <div class="news-details-wrapper">
+                <div class = 'news-date'>
+                    <?php echo $publishDate;?>
+                </div>
+                <div class = 'news-title'>
+                  <p><?php echo get_the_title();?></p>
+                </div>
+                <div class = 'news-excerpt'>
+                    <?php the_excerpt();?>
+                </div> 
+                <div class = 'news-readmore'>
+                <a href="#">Read More</a>
+                </div>     
+            </div>            
         </div>
         <?php    }
         wp_reset_query(); ?>
