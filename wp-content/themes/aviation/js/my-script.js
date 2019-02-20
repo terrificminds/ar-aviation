4cf70a56cd0dcc9f68b5946307ecbe17a96fca06
Nisful Laila M. I <nisful@terrificminds.com>
2019-02-20 18:35:20 +0530

    Missing code and style added

--- wp-content/themes/aviation/js/my-script.js
+++ wp-content/themes/aviation/js/my-script.js
@@ -82,4 +82,18 @@ jQuery(document).ready(function ($) {
     }
   }) 
 
+  $(document).ready(function(){
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.in").each(function(){
+      $(this).siblings(".panel-heading").find(".glyphicon").addClass("glyphicon-minus").removeClass("glyphicon-plus");
+    });
+    
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function(){
+      $(this).parent().find(".glyphicon").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+    }).on('hide.bs.collapse', function(){
+      $(this).parent().find(".glyphicon").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+    });
+  });
+
 });
