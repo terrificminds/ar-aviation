1187c81b5110186e8e88c20b5c32b03951b1762f
sangeetha-dev <sangeetha@terrificminds.com>
2019-02-22 14:53:07 +0530

    ARA-42 #comment 404 page change

--- wp-content/themes/aviation/404.php
+++ wp-content/themes/aviation/404.php
@@ -12,6 +12,8 @@ get_header(); ?>
 				<div class="page-content">
 				<div class="common-content">
 					<img class="404-image" src="<?php echo wp_get_attachment_url(get_theme_mod('404-image')) ?>"/>
+					<h1>Error 404.</h1>
+					<h3>Flight not found !</h3>
 					</div>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
