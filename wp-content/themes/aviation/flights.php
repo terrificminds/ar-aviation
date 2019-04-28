dc0e66e923f48155eafca619b48a7614449685a1
Nisful Laila M. I <nisful@terrificminds.com>
2019-04-28 15:06:21 +0530

    flights page - desktop and responsive styling

--- wp-content/themes/aviation/flights.php
+++ wp-content/themes/aviation/flights.php
@@ -4,51 +4,69 @@
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<?php while ( have_posts() ) : the_post();?>
-		<div class="flights-content-wrapper">
+	<?php while ( have_posts() ) : the_post();?>
+	<div class="flights-content-wrapper">
 			<div class="flights-container">
-				<div class="Flights-content">
+				<div class="flights-content">
 					<?php $flights = new WP_Query(array(
-						'post_type' => 'flights',
-						'post_status' => 'publish',
-						'posts_per_page' => -1,
-					));
-					?>
-					<?php while ($flights->have_posts()):
-						$flights->the_post();
-						$post_id = get_the_ID();
-						$featuredContent = get_post_meta(get_the_ID(), 'flight_featured_text', TRUE);
-						$chart1Link = get_post_meta(get_the_ID(), 'flights_chart_1', TRUE);
-						$chart2Link = get_post_meta(get_the_ID(), 'flights_chart_2', TRUE);
-						$featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );  
-					?>
-						<div class = "flights-block">
-							<div class = "flight-title">
-								<?php echo get_the_title();?>
-							</div>
-							<div class = "flight-featured-image">
-								<img class = "flight-featured-image" src="<?php echo $featuredImage[0]; ?>">
-							</div>    
-							<div class = "flight-featured-text">
-								<p><?php echo $featuredContent;?></p>
-							<div>
-							<div class = "flight-chart-1">
-								<img src="<?php echo $chart1Link?>">
-							</div>
-							<div class = "flight-bottom-content">
-								<div class = "flight-chart-2">
-									<img src="<?php echo $chart2Link?>">
+										'post_type' => 'flights',
+										'post_status' => 'publish',
+										'posts_per_page' => -1,
+									));
+									$count=0;
+									?>
+						 	<div class="flight-tab-slider" role="tabpanel">								
+								<ul class="nav nav-tabs" role="tablist">
+									<!-- <li class="active"><a href="#test" role="tab" data-toggle="tab">test</a></li> -->
+										<?php while ($flights->have_posts()):
+												$flights->the_post();
+												$post_id = get_the_ID();
+												$count+=1;
+											?>
+											<li class="<?php if($count==1){ echo "active";} ?>"><a href="#tab-<?php echo $post_id;?>" role="tab" data-toggle="tab"><?php echo get_the_title();?></a></li>
+										 
+									
+											<?php endwhile;	?>	
+								</ul>
+								<div class="tab-content">
+									<!-- <div role="tabpanel" class="tab-pane active" id="test">Sunday</div> -->
+									<?php 	$count=0; while ($flights->have_posts()):
+										$flights->the_post();
+										$post_id = get_the_ID();
+										$featuredContent = get_post_meta(get_the_ID(), 'flight_featured_text', TRUE);
+										$chart1Link = get_post_meta(get_the_ID(), 'flights_chart_1', TRUE);
+										$chart2Link = get_post_meta(get_the_ID(), 'flights_chart_2', TRUE);
+										$featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );
+									
+									?>
+									<div role="tabpanel" class="tab-pane <?php if($count==0){ echo "active";} $count++ ?>" id="tab-<?php echo $post_id;?>">
+										<div class = "flight-featured-image">
+											<img src="<?php echo $featuredImage[0]; ?>">
+										</div>    
+										<div class = "flight-featured-text">
+											<p><?php echo $featuredContent;?></p>						 
+											<div class = "flight-chart-1">
+												<img src="<?php echo $chart1Link?>">
+											</div>
+											<div class = "flight-bottom-content">
+												<div class = "flight-chart-2">
+													<img src="<?php echo $chart2Link?>">
+												</div>
+												<div class = "flight-body">
+													<?php the_content();?>
+												</div>
+											</div>
+										</div>	
+									</div>
+									<?php endwhile;	?>								 
 								</div>
-								<div class = "service-body">
-									<?php the_content();?>
-								</div>
-							</div>
 						</div>
-					<?php endwhile;	?>
 				</div>
 			</div>
-		</div>
-		<?php endwhile;?>	
+		</div>		
+	<?php endwhile;?>
+
+	
 	</main><!-- .site-main -->
 </div><!-- .content-area -->
 <?php get_footer(); ?>
