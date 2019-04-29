7fa7ce2830104ee4ffa1a41447820ee987b75003
Nisful Laila M. I <nisful@terrificminds.com>
2019-04-29 23:36:02 +0530

    ARA-197 Calculate page - desktop and responsive styling completed

---
+++ wp-content/themes/aviation/calculate.php
@@ -0,0 +1,33 @@
+<?php /* Template Name: Calculate*/ ?>
+
+<?php get_header();?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	
+	<div class="calculate-content-wrapper">
+		<div class="calculate-container">
+		<?php
+			// Start the loop.
+			while ( have_posts() ) : the_post();
+
+				// Include the page content template.
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+				// End of the loop.
+			endwhile;
+			?>
+		</div>
+	</div>
+		
+
+	</main><!-- .site-main -->
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
