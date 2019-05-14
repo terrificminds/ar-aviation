da6b4c82b89dade89b8ad2932078da41d0d107f7
nithin <nithin@terrificminds.com>
2019-05-14 10:36:24 +0530

    edited the custom calculator

--- wp-content/plugins/converter/js/conversion.js
+++ wp-content/plugins/converter/js/conversion.js
@@ -4,7 +4,6 @@ jQuery(document).ready(function ($) {
 var operatorValue;
 setLable(); 
 setCalculator();
-$('.remove_field').hide();
 $('.weight-calculation').on('change', function() {
   setLable(); 
   $('.convert-into').val("");
@@ -12,13 +11,21 @@ $('.weight-calculation').on('change', function() {
 
 $('.aviation-calculation').on('change', function() {
   setCalculator();
+  resetCalculator();
+});
+
+$('.reset-calculator').on('click', function() {
+  resetCalculator();
+});
+
+function resetCalculator(){
   $('.aviation-calculation-wrapper').find(':input').each(function () {
     switch (this.type) {
       case 'text': $(this).val('');
       break;
     }
   });
-});
+}
 
 function setCalculator(){
   var calculator = $('.aviation-calculation').val();
@@ -88,7 +95,7 @@ $(".convert-weight").click(function(){
   if(isNaN(result)){
     alert('Please enter a valid input');
    }else{
-    $('.convert-into').val(result.toFixed(2));
+    $('.convert-into').val(result.toFixed(4));
   }
 });
 
@@ -96,41 +103,50 @@ $(".cbm-add-row").click(function(){
   var rowcount = (parseInt($('.cbm-calculate').attr("data-id"))+1);
   var html ="";
   html += '<div class="cbm-row cbm-row-'+rowcount+'">';
-		html += '<span><label for="cbmlength-'+rowcount+'">Length</label>';
-		html += '<input type="text" name="cbmlength-'+rowcount+'" class ="cbmlength-'+rowcount+'" placeholder="100"></span>';
-		html += '<span><label for="cbmbreadth-'+rowcount+'">Breadth</label>';
-		html += '<input type="text" name="cbmbreadth-'+rowcount+'" class ="cbmbreadth-'+rowcount+'" placeholder="100"></span>';
-		html += '<span><label for="cbmwidth-'+rowcount+'">Width</label>';
-    html += '<input type="text" name="cbmwidth-'+rowcount+'" class ="cbmwidth-'+rowcount+'" placeholder="100"></span>';
-    html += '<span><label for="cbmpieces-'+rowcount+'">Pieces</label>';
-		html += '<input type="text" name="cbmpieces-'+rowcount+'" class ="cbmpieces-'+rowcount+'" placeholder="100"></span>';
-		html += '<span><label for="cbmvolume-'+rowcount+'">Volume</label>';
-		html += '<input type="text" name="cbmvolume-'+rowcount+'" class ="cbmvolume-'+rowcount+'" placeholder="100"></span>';
-		html += '<span><label for="cbmcbm-'+rowcount+'">CBM</label>';
-    html += '<input type="text" name="cbmcbm-'+rowcount+'" class ="cbmcbm-'+rowcount+'" placeholder="100"></span>';
+		html += '<span><label for="cbmlength-1">Length</label>';
+		html += '<input type="text" name="cbmlength-1" class ="cbmlength-1" placeholder="0" maxlength="10"></span>';
+		html += '<span><label for="cbmbreadth-1">Breadth</label>';
+		html += '<input type="text" name="cbmbreadth-1" class ="cbmbreadth-1" placeholder="0" maxlength="10"></span>';
+		html += '<span><label for="cbmwidth-1">Width</label>';
+    html += '<input type="text" name="cbmwidth-1" class ="cbmwidth-1" placeholder="0" maxlength="10"></span>';
+    html += '<span><label for="cbmpieces-1">Pieces</label>';
+		html += '<input type="text" name="cbmpieces-1" class ="cbmpieces-1" placeholder="0" maxlength="10"></span>';
+		html += '<span><label for="cbmvolume-1">Volume</label>';
+		html += '<input type="text" name="cbmvolume-1" class ="cbmvolume-1" placeholder="0" readonly></span>';
+		html += '<span><label for="cbmcbm-1">CBM</label>';
+    html += '<input type="text" name="cbmcbm-1" class ="cbmcbm-1" placeholder="0" readonly></span>';
+    html += '<input type="button" value="Remove" class="remove_field_1"/>';
   html += '</div>';
   $('.cbm-calculate').attr("data-id",rowcount);
   $('.cbm-table').append(html);
-  $('.remove_field').show();
 });
+
+$(document).on('click', '.remove_field_1', function (event) {
+  alert('send');
+  $(this).parent('div').remove();
+  var rowcount = (parseInt($('.cbm-calculate').attr("data-id"))-1);
+  $('.cbm-calculate').attr("data-id",rowcount);
+});  
 
 $(".cbm-calculate").click(function(){
     var totalVolume = 0;
     var totalCbm = 0;
+    var volume;
+    var cbm;
     var rows = $('.cbm-calculate').attr("data-id");
-    for(var i=1;i<=rows;i++){
-      var length = $('.cbmlength-'+i).val();
-      var breadth = $('.cbmbreadth-'+i).val();
-      var width = $('.cbmwidth-'+i).val();
-      var pieces = $('.cbmpieces-'+i).val();
-      var volume = (length*breadth*width*pieces)/6000;
-      var cbm = volume.toFixed(2)/166.66;
-      $('.cbmvolume-'+i).val(volume.toFixed(2));
-      $('.cbmcbm-'+i).val(cbm.toFixed(2));
+    var length = $('.cbmlength-1').map(function() { return this.value; }).get();
+    var breadth = $('.cbmbreadth-1').map(function() { return this.value; }).get();
+    var width = $('.cbmwidth-1').map(function() { return this.value; }).get();
+    var pieces = $('.cbmpieces-1').map(function() { return this.value; }).get();
+    for(var i=0;i<rows;i++){
+      volume = (length[i]*breadth[i]*width[i]*pieces[i])/6000;
+      cbm = volume.toFixed(2)/166.66;
+      $('.cbmvolume-1').eq(i).val(volume.toFixed(4));
+      $('.cbmcbm-1').eq(i).val(cbm.toFixed(4));
     }
-    for(var j=1;j<=rows;j++){
-      totalVolume = totalVolume + parseFloat($('.cbmvolume-'+j).val());   
-      totalCbm = totalCbm + parseFloat($('.cbmcbm-'+j).val());     
+    for(var j=0;j<rows;j++){
+      totalVolume = totalVolume + parseFloat($('.cbmvolume-1').eq(j).val());   
+      totalCbm = totalCbm + parseFloat($('.cbmcbm-1').eq(j).val());     
     }
     if(isNaN(totalVolume) || isNaN(totalCbm)){
       $('.cbm-total-volume').val('');
@@ -141,18 +157,9 @@ $(".cbm-calculate").click(function(){
       }
       alert('Please enter a valid input');
      }else{
-      $('.cbm-total-volume').val(totalVolume.toFixed(2));
-      $('.cbm-total').val(totalCbm.toFixed(2));
+      $('.cbm-total-volume').val(totalVolume.toFixed(4));
+      $('.cbm-total').val(totalCbm.toFixed(4));
     }
-});
-
-$('.remove_field').click(function(){
-  var lastrow = (parseInt($('.cbm-calculate').attr("data-id")));
-  $('.cbm-row-'+lastrow).remove();
-  if(lastrow==2){
-    $('.remove_field').hide();
-  }
-  $('.cbm-calculate').attr("data-id",lastrow-1);
 });
 
 $(".nas-calculate").click(function(){
@@ -163,13 +170,13 @@ $(".nas-calculate").click(function(){
     thc = 2.000;    
   } else if (weightInKG <= 1000 && weightInKG > 100){
     thc = 2+(-(Math.floor(-(weightInKG-100)/50))*0.5);
-    thc = thc.toFixed(3);
+    thc = thc.toFixed(4);
   }else if (weightInKG <= 5000 && weightInKG > 1000){
     thc = 2+9+(-(Math.floor(-(weightInKG-1000)/50))*0.25);
-    thc = thc.toFixed(3);
+    thc = thc.toFixed(4);
   } else if (weightInKG > 5000){
     thc = 2+9+20+(-(Math.floor(-(weightInKG-5000)/50))*0.15);
-    thc = thc.toFixed(3);
+    thc = thc.toFixed(4);
   }
   if(isNaN(thc)){
     $('.nas-formula').val('');
